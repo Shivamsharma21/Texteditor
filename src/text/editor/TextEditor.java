4ba3f05ec72f13accb45e3459617b9716f6a620e
shivam <shivam@kali>
2020-03-30 10:53:10 +0530

    first Commit

---
+++ src/text/editor/TextEditor.java
@@ -0,0 +1,18 @@
+
+package text.editor;
+
+/**
+ *
+ * @author shivam
+ */
+public class TextEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            
+    }
+    
+}
