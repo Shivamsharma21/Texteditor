47f51695a7868c4afe17c57519a9ae3b5668dc78
shivam <shivam@kali>
2020-03-30 12:58:04 +0530

    (new file)Text editor is init in Main Function

--- src/text/editor/TextEditor.java
+++ src/text/editor/TextEditor.java
@@ -13,6 +13,9 @@ public class TextEditor {
     public static void main(String[] args) {
         // TODO code application logic here
             TextEditorGUI texteditor = new TextEditorGUI();
+            texteditor.setVisible(true);
+            texteditor.setBounds(40,100,400,400);
+            texteditor.setTitle("Xpad");
     }
     
 }
